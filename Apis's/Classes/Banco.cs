91bb3f21ede98368181f984cdcf0d3305fb54958
Renato <vieirarenato843@gmail.com>
2021-07-12 09:24:42 -0300

    Adicionar arquivos de projeto.

---
+++ Apis's/Classes/Banco.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Apis_s.Classes
+{
+    public static class Banco
+    {
+        public static MySqlCommand Abrir()
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            string strConn = @"server=127.0.0.1;database=mydb;user id=root;password=usbw;port=3306";
+            MySqlConnection cn = new MySqlConnection(strConn);
+            try
+            {
+                if (cn.State != ConnectionState.Open)
+                {
+                    cn.Open();
+                    cmd.Connection = cn;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return cmd;
+        }
+    }
+}
